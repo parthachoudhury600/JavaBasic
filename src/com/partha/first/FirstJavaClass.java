e418e3c1062326b36b8f5e676ef5ba31c305cf6c
parthachoudhury600 <parthachoudhury600@gmail.com>
2022-03-31 00:56:57 +0530

    Test Commit

---
+++ src/com/partha/first/FirstJavaClass.java
@@ -0,0 +1,12 @@
+package com.partha.first;
+
+public class FirstJavaClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello Partha");
+
+	}
+
+}
